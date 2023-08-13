cfc82f0ae5473f48047c8a8d41f43277e17fa290
Jonathan Gómez <104044651+JonathanG0m3z@users.noreply.github.com>
2023-08-12 21:58:08 -0500

    google auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,9 @@ import '@sweetalert2/theme-dark';
 import '@/styles/sweetalert2.css';
 import type { AppProps } from 'next/app';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+
+const NEXT_PUBLIC_GOOGLE_CLIENT_ID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
 
 const defaultTheme = createTheme({
   palette: {
@@ -13,7 +16,9 @@ const defaultTheme = createTheme({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Component {...pageProps} />
+      <GoogleOAuthProvider clientId={NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? ''}>
+        <Component {...pageProps} />
+      </GoogleOAuthProvider>
     </ThemeProvider>
   );
 }
